8bc98c4badd7dd057628173e411221079b94b3ef
Davide Dal Pos <daveliga@gmail.com>
2025-12-08 15:02:32 -0500

    nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
+  ssr: false, // ✅ disable SSR for GitHub Pages
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
 
